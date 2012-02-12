ef05e5f356918c3c37f3b28ae7d9e3a4868f2edc
Alexander Wang <awang@awang-thinker.(none)>
2012-02-12 13:13:46 +0800

    Removed dependency of log4j.

--- project.clj
+++ project.clj
@@ -2,10 +2,6 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [log4j "1.2.15" :exclusions [javax.mail/mail
-                                              javax.jms/jms
-                                              com.sun.jdmk/jmxtools
-                                              com.sun.jmx/jmxri]]
                  [seesaw "1.3.0"]]
   :dev-dependencies [[swank-clojure "1.3.4"]
                      [com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
