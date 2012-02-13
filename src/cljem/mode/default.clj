e6257cbbe9936598e9e02a91b6efbd5c0239e09d
Alexander Wang <awang@awang-thinker.(none)>
2012-02-13 09:03:49 +0800

    Changed to let internal frame gain the focus whenever desktop gains the focus.

--- src/cljem/mode/default.clj
+++ src/cljem/mode/default.clj
@@ -1,6 +1,7 @@
 (ns cljem.mode.default
   (:require cljem.modef)
   (:use [cljem mdi]
+        [clojure.tools.logging :only [debug]]
         [seesaw core]))
 
 ;; the default mode which is the parent of all other modes
@@ -16,9 +17,15 @@
       "DONE")
     "No active frame!"))
 
+(defn- on-focus-gained [e]
+  (debug "Desktop gained the focus")
+  (if-let [frm (.getSelectedFrame (to-widget e))]
+    (.requestFocusInWindow frm)))
+
 (cljem.modef/init-mode
  :display-name "default"
  :keymap {"D" #'close-active-frame}
  :widget (desktop-pane :id :desktop
                        :focusable? true
+                       :listen [:focus-gained on-focus-gained]
                        :background "gray"))
