2f67cc174967a340165ceba732c75b6e22d997f2
Alexander Wang <awang@awang-thinker.(none)>
2013-02-03 23:00:54 +0800

    Upgraded to clojure 1.4.0.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cljem "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.logging "0.2.3"]
-                 [seesaw "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.logging "0.2.4"]
+                 [seesaw "1.4.2"]]
   :dev-dependencies [[swank-clojure "1.3.4"]
                      [com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
